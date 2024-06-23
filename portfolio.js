3a230ae4bd7ee2c1188da9855d9e5744e2dd08d7
nishantmathireddi <151186092+nishantmathireddi@users.noreply.github.com>
2024-06-23 22:19:02 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    var typed = new Typed(".typing", {
+        strings: [" WEB developer"],
+        typeSpeed: 150,
+        backSpeed: 150,
+        loop: true
+    });
+});
